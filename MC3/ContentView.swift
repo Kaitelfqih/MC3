40a58e1962f35da96b3418952011ddbaae207f53
khadija <aitelfqih.khadija@gmail.com>
2024-02-19 10:15:12 +0100

    Initial Commit

---
+++ MC3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC3
+//
+//  Created by khadija Ait ElFqih on 19/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC3/MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3App.swift
+//  MC3
+//
+//  Created by khadija Ait ElFqih on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
